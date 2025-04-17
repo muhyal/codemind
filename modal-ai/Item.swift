df6581d259e808e8ddff0219ef1eea8db6834e41
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2025-04-17 15:15:51 +0300

    Initial Commit

---
+++ modal-ai/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  modal-ai
+//
+//  Created by Muhammed Yalçınkaya on 17.04.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
